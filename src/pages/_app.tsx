f115fb38bac5b4b2e0e5ecf37e03607f542a28c1
pvwnthem <pvwn.them@gmail.com>
2023-01-30 21:58:02 -0500

    add session and nextauth.ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
+
+import { SessionProvider } from "next-auth/react"
+import type { Session } from "next-auth"
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// `useSession()` anywhere in your application to access the `session` object.
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
