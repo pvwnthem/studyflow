f70e061fd04912283dfbdb04fbb509a809c0a0a1
pvwnthem <pvwn.them@gmail.com>
2023-01-30 20:03:22 -0500

    bigger text, name links

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,14 +5,14 @@ export default function Nav() {
     return (
         <>
             <div className="w-full h-24 border flex">
-                <div className="w-1/3 bg-blue-200"></div> {/* left box, padding only, no content */}
+                <div className="w-1/3"></div> {/* left box, padding only, no content */}
                 <div className="w-1/3 flex items-center justify-center"> {/* center box with logo text*/}
                     <h1 className="tenorSans text-6xl text-center">Studyflow</h1>
                 </div>
                 <div className="w-1/3 flex items-center justify-around"> {/* right box with links */}
-                    <NavText>home</NavText>
-                    <NavText>home</NavText>
-                    <NavText>home</NavText>
+                    <NavText>Home</NavText>
+                    <NavText>Solutions</NavText>
+                    <NavText>About</NavText>
                     <NavText>Get Started</NavText>
                 </div>
             </div>
--- src/components/wrappers/NavText.tsx
+++ src/components/wrappers/NavText.tsx
@@ -2,7 +2,7 @@
 export default function NavText(props: any) {
 
     return (
-        <h1 className="tenorSans text-xl hover-underline-animation">
+        <h1 className="tenorSans text-2xl hover-underline-animation">
             {props.children}
         </h1>
     )
