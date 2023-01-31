722540f8813b3aba4856eb371de1140d60376c5d
pvwnthem <pvwn.them@gmail.com>
2023-01-30 21:11:42 -0500

    remove space

--- src/components/splashPage.tsx
+++ src/components/splashPage.tsx
@@ -6,7 +6,7 @@ export default function SplashPage() {
         <>
             <div className='w-full h-full sp absolute flex text-center justify-center items-center'>
                 <ReactTypingEffect className='tenorSans lg:text-9xl text-5xl' text={["Study", "Learn" ,"Plan", "Manage", "Perform"]} speed={175} typingDelay={500}  eraseDelay={1000} cursor=" "/>
-                <p className='tenorSans lg:text-9xl text-5xl'> Better</p>
+                <p className='tenorSans lg:text-9xl text-5xl'>Better</p>
 
             </div>
         </>
