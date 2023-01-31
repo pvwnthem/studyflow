92dd7368c58f9d196a8938d2e0594de1029183fb
pvwnthem <pvwn.them@gmail.com>
2023-01-30 21:08:25 -0500

    fix text sizing on different screens

--- src/components/splashPage.tsx
+++ src/components/splashPage.tsx
@@ -5,8 +5,8 @@ export default function SplashPage() {
     return (
         <>
             <div className='w-full h-full sp absolute flex text-center justify-center items-center'>
-                <ReactTypingEffect className='tenorSans text-9xl' text={["Study", "Learn" ,"Plan", "Manage", "Perform"]} speed={175} typingDelay={500}  eraseDelay={1000} cursor="|"/>
-                <p className='tenorSans text-9xl'>Better</p>
+                <ReactTypingEffect className='tenorSans lg:text-9xl text-5xl' text={["Study", "Learn" ,"Plan", "Manage", "Perform"]} speed={175} typingDelay={500}  eraseDelay={1000} cursor=" "/>
+                <p className='tenorSans lg:text-9xl text-5xl'> Better</p>
 
             </div>
         </>
