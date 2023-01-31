e6ada997010ad2aae3df90af85a10ae11dedae74
pvwnthem <pvwn.them@gmail.com>
2023-01-30 22:20:06 -0500

    conditionally render session

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
 import SplashPage from "@components/splashPage"
 import Nav from "@components/nav"
 import Info from "@/components/info"
+import { useSession } from 'next-auth/react'
 
 export default function Home() {
+  const { data: session } = useSession()
+
+  
   return (
     <>
+    {session ? (
+      <>
+      <Nav />
+      
+      <p>{JSON.stringify(session)}</p>
+      </>
+    ): (
+      <>
       <div className='w-full h-screen '>
         <Nav />
         <SplashPage />
@@ -14,6 +26,9 @@ export default function Home() {
         <Info />
         
       </div>
+      </>
+    )}
+      
 
 
 
