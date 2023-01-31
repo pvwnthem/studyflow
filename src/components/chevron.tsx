3b4d8d58a05bc14bc33241f653ce42ebafa4c686
pvwnthem <pvwn.them@gmail.com>
2023-01-30 21:26:10 -0500

    add grid format and down cheveron

---
+++ src/components/chevron.tsx
@@ -0,0 +1,8 @@
+export default function Chevron() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 ">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
+</svg>
+
+    )
+}
--- src/components/splashPage.tsx
+++ src/components/splashPage.tsx
@@ -1,14 +1,27 @@
 import ReactTypingEffect from 'react-typing-effect';
-
+import Chevron from '@components/chevron';
 
 export default function SplashPage() {
     return (
         <>
-            <div className='w-full h-full sp absolute flex text-center justify-center items-center'>
-                <ReactTypingEffect className='tenorSans lg:text-9xl text-5xl' text={["Study", "Learn" ,"Plan", "Manage", "Perform"]} speed={175} typingDelay={500}  eraseDelay={1000} cursor=" "/>
-                <p className='tenorSans lg:text-9xl text-5xl'>Better</p>
+            <div className='w-full h-full sp absolute flex flex-col text-center justify-center items-center'>
+                <div className='h-1/3 w-full flex justify-center items-center'>
 
+                </div>
+                <div className='h-1/3 w-full flex justify-center items-center'>
+                    <ReactTypingEffect className='tenorSans lg:text-9xl text-5xl' text={["Study", "Learn" ,"Plan", "Manage", "Perform"]} speed={175} typingDelay={500}  eraseDelay={1000} cursor=" "/>
+                    <p className='tenorSans lg:text-9xl text-5xl'>Better</p>
+                </div>
+                <div className='h-1/3 w-full flex justify-center items-center'>
+                    <div className='mt-auto py-10 '>
+                        <Chevron />
+                    </div>
+                </div>
+                
+                
+                
             </div>
+            
         </>
     )
     
