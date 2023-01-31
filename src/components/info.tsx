4fd4df40c5dcc9850a4365a61de4f62259b16fd3
pvwnthem <pvwn.them@gmail.com>
2023-01-30 21:29:04 -0500

    start info page

---
+++ src/components/info.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+class Info extends React.Component {
+    constructor(props: any) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <>
+            </>
+      
+        );
+  }
+}
+
+export default Info;
