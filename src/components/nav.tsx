8476da0ca5cc003d4261328d1c54b1bec6f74859
pvwnthem <pvwn.them@gmail.com>
2023-01-30 19:43:34 -0500

    center logo, comments

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,9 +4,9 @@ export default function Nav() {
     return (
         <>
             <div className="w-full h-24 border flex">
-                <div className="w-1/3 bg-blue-200"></div>
-                <div className="w-1/3">
-                    <p className="tenorSans">Studyflow</p>
+                <div className="w-1/3 bg-blue-200"></div> {/* left box, padding only, no content */}
+                <div className="w-1/3 flex items-center justify-center"> {/* center box with logo text*/}
+                    <h1 className="tenorSans text-6xl text-center">Studyflow</h1>
                 </div>
                 <div className="w-1/3 bg-blue-400"></div>
             </div>
